026e8d53305f7c86ae60b8dfed2219a965e743c8
menchumenx <menchumenx@gmail.com>
2022-09-20 14:50:16 +0200

    añadimos reto2

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function suma(num1, num2) {
+    let res;
+    let suma = num1+num2;
+    res = suma
+    return res;
+}
+
+function resta(num1, num2) {
+    let res;
+    let resta = num1-num2;
+    res = resta;
+    return res;
+}
+
+function multiplicar(num1,num2) {
+    let res;
+    let multiplicar = num1*num2;
+    res = multiplicar;
+    return res;
+}
+
+function dividir(num1,num2) {
+    let res;
+    let dividir = num1/num2;
+    res = dividir;
+    return res;
+}
+
+//exportación de funciones
+module.exports = {suma, resta, multiplicar, dividir};
---
+++ run.js
@@ -0,0 +1,9 @@
+
+// importación de archivo de funciones
+let myLib = require('./index');
+
+// llamada a las funciones
+myLib.suma(2,5);
+myLib.resta(5,3);
+myLib.multiplicar(2,2);
+myLib.dividir(10,2);
